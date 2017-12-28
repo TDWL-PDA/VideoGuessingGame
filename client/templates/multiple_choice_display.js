8740dcb1903ada0b9c7d194534ed5272c267d8a8
Patrick Sobolewski <psobolew@mymail.mines.edu>
2017-12-27 18:29:10 -0600

    Limited voting to once.

--- client/templates/multiple_choice_display.js
+++ client/templates/multiple_choice_display.js
@@ -6,6 +6,7 @@ import './multiple_choice_display.html';
 Template.multiple_choice_display.onCreated(function scoreDisplayOnCreated() {
   this.score1 = new ReactiveVar(0);
   this.score2 = new ReactiveVar(0);
+  this.hasVoted = new ReactiveVar(false);
 });
 
 Template.multiple_choice_display.helpers({
@@ -19,12 +20,18 @@ Template.multiple_choice_display.helpers({
 
 Template.multiple_choice_display.events({
   'click .choices'(event, instance) {
-  	// TODO: Only allowed to submit once
+  	if (instance.hasVoted.get()) {
+  		window.alert("Already voted on this question, scrub.");
+  		return;
+  	}
+
   	if (event.target.value == "South Park") {
 	  	window.alert("You are right!!");
 	    instance.score1.set(instance.score1.get() + 1);
 	} else {
 		window.alert("You are WRONG!!!!");
 	}
+
+	instance.hasVoted.set(true);
   },
 });
